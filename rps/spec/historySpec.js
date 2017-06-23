2d67ae28656ab1b175b4de6bed9ae53051f010e3
mparker <mparker@pivotal.io>
2017-06-23 15:12:46 -0400

    contract

--- rps/spec/historySpec.js
+++ rps/spec/historySpec.js
@@ -20,11 +20,7 @@ describe("history", function () {
             let playRoundUI = { p1Wins(){} }
             let ui = jasmine.createSpyObj("ui", ["rounds"])
 
-            let roundRepo = {
-                isEmpty(){},
-                getAll(){},
-                save(){}
-            }
+            let roundRepo = new FakeRoundRepo()
 
             new RPS().playRound("rock", "scissors", playRoundUI, roundRepo)
 
@@ -34,6 +30,59 @@ describe("history", function () {
                 new Round("rock", "scissors", "p1")
             ])
         })
-
     })
 })
+
+function FakeRoundRepo(){
+    let rounds = []
+
+    this.isEmpty = function(){
+        return rounds.length === 0
+    }
+
+    this.save = function(r){
+        rounds.push(r)
+    }
+
+    this.getAll = function(){
+        return rounds
+    }
+}
+
+function roundRepoContract(roundRepoClass){
+    describe("round repo", function () {
+        let roundRepo
+
+        beforeEach(function () {
+            roundRepo = new roundRepoClass()
+        })
+
+        describe("when rounds have been saved", function () {
+            it("returns the saved rounds", function () {
+                let round1 = new Round(Math.random())
+                let round2 = new Round(Math.random())
+
+                roundRepo.save(round1)
+                roundRepo.save(round2)
+
+                expect(roundRepo.getAll()).toContain(round1, round2)
+            })
+
+            it("is not empty", function () {
+                roundRepo.save(new Round())
+
+                expect(roundRepo.isEmpty()).toBe(false)
+            })
+        })
+
+        describe("when no rounds have been saved", function () {
+            it("is empty", function () {
+                expect(roundRepo.isEmpty()).toBe(true)
+            })
+        })
+
+    })
+}
+
+
+roundRepoContract(FakeRoundRepo)
